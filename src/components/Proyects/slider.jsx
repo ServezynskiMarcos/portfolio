c6c9c7d32b55c1e416decbcf7577a8c06f5941a3
Servezynski <servezynskimarcos1@gmail.com>
2022-09-21 18:19:29 -0300

    orden

--- src/components/Proyects/slider.jsx
+++ src/components/Proyects/slider.jsx
@@ -2,25 +2,28 @@ import img1 from "./img/img1.jpg";
 import img2 from "./img/img2.jpg";
 import img3 from "./img/img3.jpg";
 export const SliderDate = [
-    {
-        image: img1,
-        tittle: "B2B COMMODITIES",
-        description: "Haz crecer tu negocio a nivel internacional, nosotros nos encargamos de facilitar contactos alrededor del mundo. Al combinar publicaciones de compra y venta de productos básicos, le brindamos una plataforma para comunicarse con corredores y vendedores/compradores de primer nivel. Ten todos los contactos que necesitas en una sola plataforma, trabaja desde la comodidad... con un solo click conéctate a todo el comercio internacional.",
-        deploy: "https://dev-commodities.vercel.app/",
-        repo: "https://github.com/ServezynskiMarcos/Commodities-Project.git"
-    },
-    {
-        image: img2,
-        tittle: "Single Page Application",
-        description: "World Api es una SPA, que tiene como fin poder brindar información de todos los países del mundo, contando con ordenamiento por cantidad de habitantes, contiente y más!. A demás el usuario tiene la posibilidad de interactuar con la pagina, creando actividades turísticas en algún país en particular, esta pagina fue creada consumiendo datos de la ya conocida API rest countries.",
-        deploy: "https://res-countries-api.vercel.app/",
-        repo: "https://github.com/ServezynskiMarcos/Countries-PI"
-      },
-      {
-        image: img3,
-        tittle: "Venice Resort SPA",
-        description: "Se trata de una pagina de reserva hotelera, la misma cuenta con calendario de reserva, apartado de ofertas, galería de imágenes, formulario de contacto y más!",
-        deploy: "https://venice-resort.vercel.app/",
-        repo: "https://github.com/ServezynskiMarcos/resortvenice"
-      },
-]
+  {
+    image: img3,
+    tittle: "Venice Resort SPA",
+    description:
+      "Se trata de una pagina de reserva hotelera, la misma cuenta con calendario de reserva, apartado de ofertas, galería de imágenes, formulario de contacto y más!",
+    deploy: "https://venice-resort.vercel.app/",
+    repo: "https://github.com/ServezynskiMarcos/resortvenice",
+  },
+  {
+    image: img2,
+    tittle: "Single Page Application",
+    description:
+      "World Api es una SPA, que tiene como fin poder brindar información de todos los países del mundo, contando con ordenamiento por cantidad de habitantes, contiente y más!. A demás el usuario tiene la posibilidad de interactuar con la pagina, creando actividades turísticas en algún país en particular, esta pagina fue creada consumiendo datos de la ya conocida API rest countries.",
+    deploy: "https://res-countries-api.vercel.app/",
+    repo: "https://github.com/ServezynskiMarcos/Countries-PI",
+  },
+  {
+    image: img1,
+    tittle: "B2B COMMODITIES",
+    description:
+      "Haz crecer tu negocio a nivel internacional, nosotros nos encargamos de facilitar contactos alrededor del mundo. Al combinar publicaciones de compra y venta de productos básicos, le brindamos una plataforma para comunicarse con corredores y vendedores/compradores de primer nivel. Ten todos los contactos que necesitas en una sola plataforma, trabaja desde la comodidad... con un solo click conéctate a todo el comercio internacional.",
+    deploy: "https://dev-commodities.vercel.app/",
+    repo: "https://github.com/ServezynskiMarcos/Commodities-Project.git",
+  },
+];
